6d6e73d476f4035a13b7b61946fe68601ee0d246
Lakindu Hesara Perera <lakindupereraap@gmail.com>
2026-04-02 15:14:06 +0530

    test(build): Fixed selenium webdriver issues

--- tests/features/driver.py
+++ tests/features/driver.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+
 
 
 def get_driver(headless:bool = False) -> webdriver.Chrome:
@@ -7,7 +9,10 @@ def get_driver(headless:bool = False) -> webdriver.Chrome:
     Return a webdriver instance.
     """
     options = Options()
-    options.binary_location = r"/usr/bin/brave-browser"
+    service = Service(executable_path="/usr/bin/chromedriver")
+    options.binary_location = r"/usr/bin/chromium-browser"
+
     if headless: options.add_argument("--headless=new")
+
     options.add_experimental_option("detach", True)
-    return webdriver.Chrome(options=options)
+    return webdriver.Chrome(service=service, options=options)
